ace4826a562e0b96d7122d64e03a639553fb0605
amsidhlokhande <amsidhlokhande@gmail.com>
2021-01-11 11:13:44 +0530

    Updated pom and java file

--- SpringWebServiceAccountManagement/src/main/java/com/amsidh/mvc/routes/AccountRoute.java
+++ SpringWebServiceAccountManagement/src/main/java/com/amsidh/mvc/routes/AccountRoute.java
@@ -1,20 +1,22 @@
 package com.amsidh.mvc.routes;
 
-import com.amsidh.mvc.handler.AccountHandler;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import com.amsidh.mvc.handler.AccountHandler;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @Slf4j
 public class AccountRoute {
 
     @Bean
-    public RouterFunction getAccountRoutes(AccountHandler accountHandler){
+    public RouterFunction<ServerResponse> getAccountRoutes(AccountHandler accountHandler){
         log.info("AccountRoute getAccountRoutes method called");
         return RouterFunctions.route(RequestPredicates.GET("/accounts/health/check"), accountHandler::healthCheck);
     }
